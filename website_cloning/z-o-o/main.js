69850faffca606ba8a499a1aaca18995f0709bee
고동현 <dhjk35@naver.com>
2021-08-25 23:13:46 +0900

    add little function

---
+++ website_cloning/z-o-o/main.js
@@ -0,0 +1,17 @@
+const nav = document.querySelector(".main-nav");
+const firstNav = document.querySelector("#first-mnav");
+const secondNav = document.querySelector("#second-mnav");
+const home = document.querySelector("#home");
+const button = document.querySelector("#close")
+
+// zoo 를 클릭하면 새로고침하게 만듬
+home.addEventListener("click", () => {
+  location.reload();
+})
+
+// main-nav를 클릭하면 info 창이 켜지게
+nav.addEventListener("click", () => {
+  secondNav.classList.toggle("hidden");
+  button.classList.toggle("hidden");
+  nav.classList.toggle("active");
+})
